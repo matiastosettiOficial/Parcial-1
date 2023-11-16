1fab6f450f476793ff9926c770758f9290c387ae
Matias Tosetti Docente <141670493+matiastosettiOficial@users.noreply.github.com>
2023-11-16 07:29:27 -0300

    Initial commit

---
+++ Parcial Turnero T1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Cola/Cola.h"
+#include "Cola/ColaPrioridad.h"
+#include "Lista/CircList.h"
+#include "Lista/Lista.h"
+#include "Pila/pila.h"
+
+int main() {
+    std::cout << "cree su programa" << std::endl;
+
+    return 0;
+}
